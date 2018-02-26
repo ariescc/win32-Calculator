d930465051bffeee4373f8eefeecb17f5570e678
ariescc <jiachang@mail.nwpu.edu.cn>
2018-02-26 19:36:21 +0800

    update

---
+++ Win32Project1/zdd.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "resource.h"
+
+// ÎÄ±¾¿ò
+#define RESULT 199
+
+// Êý×Ö¼ü
+#define NUM0   200       
+#define NUM1   201
+#define NUM2   202
+#define NUM3   203
+#define NUM4   204
+#define NUM5   205
+#define NUM6   206
+#define NUM7   207
+#define NUM8   208
+#define NUM9   209
+
+// .¼ü
+#define POI    210
+
+// ÔËËã¼ü
+#define ADD    211         // +ºÅ
+#define SUB    212         // -ºÅ
+#define MUL    213         // *ºÅ
+#define DIV    214         // /ºÅ
+#define EQU	   215         // =ºÅ
+#define SQR    216         //¿ª·½
+#define MOD    217		   // %ºÅ
+
+// Çå¿Õ¼ü
+#define CLE    218
+
+#define SZSQR   "y¡Ìx"
